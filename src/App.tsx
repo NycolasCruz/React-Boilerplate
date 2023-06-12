88808cc75deb6493b4f81ba904e77752efafd16d
NycolasCruz <nycolascruzsilva@gmail.com>
2023-06-11 21:48:55 -0300

    style: small adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import { useState } from "react";
-
+import { SelectExample } from "@/components/SelectExample";
 import { Logos } from "@/components/Logos";
-import "./App.scss";
 
-function App() {
-	const [count, setCount] = useState(0);
-
+export function App() {
 	return (
 		<>
 			<Logos />
 
 			<h1>React Boilerplate</h1>
 
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+			<SelectExample />
 		</>
 	);
 }
-
-export default App;
