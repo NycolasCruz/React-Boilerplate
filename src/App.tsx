0a48db9bcb0442527c175e9afaa88b70cb5009d3
NycolasCruz <nycolascruzsilva@gmail.com>
2023-07-03 17:00:22 -0300

    chore: react pro sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { SelectExample } from "@/components/SelectExample";
 import { Logos } from "@/components/Logos";
 
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+
 export function App() {
 	return (
 		<>
+			<Sidebar>
+				<Menu>
+					<SubMenu label="Charts">
+						<MenuItem> Pie charts </MenuItem>
+						<MenuItem> Line charts </MenuItem>
+					</SubMenu>
+					<MenuItem> Documentation </MenuItem>
+					<MenuItem> Calendar </MenuItem>
+				</Menu>
+			</Sidebar>
+
 			<Logos />
-
 			<h1>React Boilerplate</h1>
-
 			<SelectExample />
 		</>
 	);
