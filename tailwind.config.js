ed6414ca279414b13c543dc0ae1db9fb26e1ec31
NycolasCruz <nycolascruzsilva@gmail.com>
2023-08-03 17:14:59 -0300

    style: format

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
 	content: [
 		"./index.html",
 		"./src/**/*.{js,ts,jsx,tsx}",
-		"node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
+		"node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
 	],
-	plugins: [plugin]
+	plugins: [plugin],
 };
