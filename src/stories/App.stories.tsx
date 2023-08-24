752a33f32ce3444e3bf64346904ab59ee9d270a0
NycolasCruz <nycolascruzsilva@gmail.com>
2023-08-24 13:32:00 -0300

    docs: small adjustment

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -5,21 +5,6 @@ import { App } from "../App";
 export default {
 	title: "App",
 	component: App,
-	decorators: [
-		(Story) => (
-			<div
-				style={{
-					display: "flex",
-					height: "95vh",
-					alignItems: "center",
-				}}
-			>
-				<div id="root">
-					<Story />
-				</div>
-			</div>
-		),
-	],
 } as Meta;
 
 export const Default = {} as StoryObj;
