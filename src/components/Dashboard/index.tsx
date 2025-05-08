80347722264e05040c9d8ac6ac43ee9dfbe1df7f
NycolasCruz <nycolascruzsilva@gmail.com>
2025-05-08 02:09:46 -0300

    feat: novo componente Dashboard

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { DefaultCard } from "@/components/DefaultCard";
+
+export function Dashboard() {
+	return (
+		<DefaultCard>
+			<DefaultCard.Title>Dashboard</DefaultCard.Title>
+			<DefaultCard.Description>Descrição da dashboard</DefaultCard.Description>
+		</DefaultCard>
+	);
+}
