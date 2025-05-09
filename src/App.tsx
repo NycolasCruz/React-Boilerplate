a47bd485f5ae5383bfa574508713b57fc8efc314
NycolasCruz <nycolascruzsilva@gmail.com>
2025-05-09 01:10:54 -0300

    refactor[wip]: adicionando roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import { Dashboard } from "@/components/Dashboard";
-import { Layout } from "@/components/Layout";
+import { useEffect } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import { Dashboard } from "./pages/Dashboard";
+import { Profile } from "./pages/Profile";
+import { Error } from "./pages/Error";
+import { Layout } from "./layout";
 
 export function App() {
-	return (
-		<Layout>
-			<Dashboard />
-		</Layout>
-	);
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Layout />,
+			errorElement: <Error />,
+			children: [
+				{ path: "/", element: <Dashboard /> },
+				{ path: "/perfil", element: <Profile /> },
+			],
+		},
+	]);
+
+	function setDefaultTheme() {
+		const html = document.getElementById("html") as HTMLHtmlElement;
+
+		html.className = "dark";
+	}
+
+	useEffect(() => {
+		setDefaultTheme();
+	}, []);
+
+	return <RouterProvider router={router} />;
 }
