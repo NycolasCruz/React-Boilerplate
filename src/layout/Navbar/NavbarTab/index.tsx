8e010b294c90d1e1291e86375740e75e8c9cef93
nycolascruz <nycolascruzsilva@gmail.com>
2025-05-09 16:41:13 -0300

    feat: adicionando Link ao NavbarTab

--- src/layout/Navbar/NavbarTab/index.tsx
+++ src/layout/Navbar/NavbarTab/index.tsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router";
+
 type Props = {
+	to: string;
 	name: string;
 };
 
-export function NavbarTab({ name }: Props) {
+export function NavbarTab({ to, name }: Props) {
 	return (
-		<button className="relative hover:text-gray-900 focus:text-gray-900 before:content-[''] before:absolute before:-bottom-[1.9rem] before:left-2/4 before:w-0 before:h-[3px] before:bg-[#8a51ff] before:duration-200 before:ease-out before:-translate-x-1/2 hover:before:w-[150%] focus:before:w-[150%] dark:hover:text-white dark:before:bg-[#ffd700] dark:focus:text-white">
+		<Link
+			to={to}
+			className="relative hover:text-gray-900 focus:text-gray-900 before:content-[''] before:absolute before:-bottom-[1.9rem] before:left-2/4 before:w-0 before:h-[3px] before:bg-[#8a51ff] before:duration-200 before:ease-out before:-translate-x-1/2 hover:before:w-[150%] focus:before:w-[150%] dark:hover:text-white dark:before:bg-[#ffd700] dark:focus:text-white"
+		>
 			{name}
-		</button>
+		</Link>
 	);
 }
