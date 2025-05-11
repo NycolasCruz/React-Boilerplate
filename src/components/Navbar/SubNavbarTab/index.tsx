cb2b9ceaca9d9e9c5deb902e850ca5d061a908c8
NycolasCruz <nycolascruzsilva@gmail.com>
2025-05-10 21:17:23 -0300

    style: corrigindo estilo das bordas do componente SubNavbarTab

--- src/components/Navbar/SubNavbarTab/index.tsx
+++ src/components/Navbar/SubNavbarTab/index.tsx
@@ -19,8 +19,12 @@ function SubNavbarTab({ tabName, children }: SubNavbarTabProps) {
 
 	return (
 		<FlowbiteTooltip
-			className="bg-gray-800 dark:bg-gray-800 border border-gray-700 dark:border-gray-700 p-0"
-			content={children}
+			className="p-0"
+			content={
+				<ul className="text-gray-400 bg-gray-800 border border-gray-700 rounded dark:bg-gray-800 dark:border-gray-700">
+					{children}
+				</ul>
+			}
 			trigger={widthBelowWide ? "click" : "hover"}
 			arrow={false}
 		>
@@ -33,13 +37,11 @@ function SubNavbarTab({ tabName, children }: SubNavbarTabProps) {
 
 function MenuItem({ to, children }: MenuItemProps) {
 	return (
-		<ul className="text-gray-400">
-			<li className="rounded-3xl">
-				<Link to={to} className="flex w-full rounded hover:bg-[#18212e] py-4 ps-5 pe-12">
-					{children}
-				</Link>
-			</li>
-		</ul>
+		<li>
+			<Link to={to} className="flex w-full rounded hover:bg-[#18212e] py-4 ps-5 pe-12">
+				{children}
+			</Link>
+		</li>
 	);
 }
 
